3d9a95239d358c4cd74205f5e93ffaef07a8af38
Rahnken <ericdonnelly4@gmail.com>
2023-11-14 18:56:17 -0500

    font awesome works now

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import { About } from "./pages/About";
 import Contact from "./pages/Contact";
 import { HighScore } from "./pages/HighScore";
 import NoPage from "./pages/NoPage";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+library.add(fab, fas, far);
 
 export default function App() {
   return (
