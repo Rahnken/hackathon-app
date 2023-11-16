ba04f42669368f6eb97a26a97e3f98ef820b9b15
Jessica Pickens <jessicapickens2498@gmail.com>
2023-11-16 02:30:05 -0700

    media-qurrys, general css

--- src/pages/HighScore.tsx
+++ src/pages/HighScore.tsx
@@ -1,7 +1,17 @@
 export function HighScore() {
   return (
     <>
-      <h2>HighScore</h2>
+      <div className="left-sidebar">
+        <img src="src\img\left-sidebar.png" alt="cartoon-collage" />
+      </div>        
+      <div className="right-sidebar">
+        <img src="src\img\right-sidebar1.png" alt="anime-collage" />
+      </div>
+      <div className="container-body">
+        <div className="body-content">
+          <h2>HighScore</h2>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,7 +1,17 @@
 export default function NoPage() {
   return (
     <>
-      <h2>Error: 404</h2>
+      <div className="left-sidebar">
+        <img src="src\img\left-sidebar.png" alt="cartoon-collage" />
+      </div>        
+      <div className="right-sidebar">
+        <img src="src\img\right-sidebar1.png" alt="anime-collage" />
+      </div>
+      <div className="container-body flex-container">
+        <div className="body-content">
+          <h2 className="glitch error">Error: 404</h2>
+        </div>
+      </div>
     </>
   );
 }
