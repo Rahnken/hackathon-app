a1ba8c8919c4fce7205f9dc5f026ef922b044784
Rahnken <ericdonnelly4@gmail.com>
2023-11-19 10:42:57 -0500

    fixed some minor issues

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Character, GameState } from "../utils/types";
 import { LayoutPage } from "../components/Layouts/LayoutPage";
 import { Link } from "react-router-dom";
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -19,6 +19,7 @@ export type Creator = {
   socials: SocialsObject[];
 };
 export type Score = {
+  id: number;
   name: string;
   value: number;
 };
